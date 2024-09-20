dfc4887ab1d6c7c59e56981faa7dba7fc7aee18a
Sabrina Matos <sabrinamatosalmeida2@gmail.com>
2024-09-20 19:03:21 -0300

    development

--- Mini_Projeto.m5/src/App.jsx
+++ Mini_Projeto.m5/src/App.jsx
@@ -1,2 +1,29 @@
+import './App.css'
 
 
+import React from 'react';  
+
+export function App() {  
+  const handleButtonClick1 = () => {  
+    alert('Botão 1 clicado!');  
+  };  
+
+  const handleButtonClick2 = () => {  
+    alert('Botão 2 clicado!');  
+  };  
+
+  return (  
+    <div style={{ textAlign: 'center', padding: '20px' }}>  
+      <h1>Sobre a Uber</h1>  
+      <p>  
+        A Uber é uma plataforma de transporte que conecta motoristas parceiros a passageiros por meio de um aplicativo.   
+        Oferece várias opções de transporte, incluindo carros, bicicletas e até entrega de refeições.  
+      </p>  
+
+      <div>  
+        <button onClick={handleButtonClick1} style={{ margin: '10px' }}>Sobre o projeto</button>  
+        <button onClick={handleButtonClick2} style={{ margin: '10px' }}>Cards</button>  
+      </div>  
+    </div>  
+  );  
+}
